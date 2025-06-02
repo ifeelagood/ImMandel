1c78b2950903f843311af790cee9d6dda54a33bc
ifeelagood <thicccatto3@gmail.com>
2025-06-02 16:22:18 +1000

    updated main appplication and CMakeLists to reflect recent commits

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,14 +1,21 @@
 #include "Application.h"
 
+#include <imgui.h>
+#include <imgui_impl_win32.h>
+#include <imgui_impl_dx11.h>
 
+#include "TransformGUI.h"
 
 Application::Application()
-	: _window(std::make_unique<Window>(L"ImMandel", 1920, 1080))
+	: _window(std::make_unique<Window>(L"ImMandel", 640, 480))
 	, _d3d(std::make_unique<D3D>(_window->get_handle()))
 	, _gui(std::make_unique<GUI>(_d3d->get_device(), _d3d->get_context(), _window->get_handle()))
+	, _transform(std::make_unique<Transform<float>>(_window->get_size()))
+	, _engine(std::make_unique<EscapeTimeEngine<float>>())
 	, _greyscale_shader(std::make_unique<Shader<GreyscaleParameters>>(_d3d->get_device(), L"res/shaders/greyscale_vs.hlsl", L"res/shaders/greyscale_ps.hlsl")) 
 
 {
+	_gui->register_drawable(std::make_unique<TransformGUI<float>>(*_transform.get()));
 }
 
 Application::~Application()
@@ -32,21 +39,22 @@ int Application::run()
 		// if window has changed size, update 
 		if (Window::resize.x() != 0 && Window::resize.y() != 0) {
 			_d3d->resize(Window::resize);
+			_transform->set_size(Window::resize);
 			Window::resize.x() = Window::resize.y() = 0;
 		}
 
 
 		_gui->new_frame();
-		_gui->show_demo_window();
+		//_gui->show_demo_window();
 
-		std::span<uint32_t> buffer = _d3d->map();
+		if (_transform->changed()) {
+			Surface<uint32_t> buffer = _d3d->map();
 
-		// dummy data
-		for (size_t i = 0; i < buffer.size(); i++) {
-			buffer[i] = rand() % _greyscale_shader->parameters().max_it;
+			_engine->compute(*_transform.get(), buffer, _greyscale_shader->parameters().max_it);
+
+			_d3d->unmap();
 		}
 
-		_d3d->unmap();
 
 		_greyscale_shader->bind(_d3d->get_context());
 		
--- src/Application.h
+++ src/Application.h
@@ -1,12 +1,19 @@
 #pragma once
 
 #include <memory>
+#define BOOST_NO_EXCEPTIONS
 
 #include "D3D.h"
 #include "Window.h"
 #include "GUI.h"
 #include "Shader.h"
 #include "ShaderParameters.h"
+#include "Transform.h"
+#include "TransformGUI.h"
+
+#include "Engine.h"
+#include "EscapeTimeEngine.h"
+
 
 
 class Application {
@@ -15,7 +22,8 @@ private:
 	std::unique_ptr<Window> _window;
 	std::unique_ptr<D3D> _d3d;
 	std::unique_ptr<GUI> _gui;
-	
+	std::unique_ptr<Transform<float>> _transform;
+	std::unique_ptr<Engine<float>> _engine;
 	std::unique_ptr<Shader<GreyscaleParameters>> _greyscale_shader;
 
 public:
